0948cc4c01dbd7ff3095439eb52b2a5b2d28c4d3
maryamali2 <101944512+maryamali2@users.noreply.github.com>
2024-04-18 00:10:02 -0400

    Delete ADC.c

--- ADC.c
+++
@@ -1,8 +0,0 @@
-#include "I2C.h"
-#include "stdbool.h"
-
-#define ADC_ADDR 0x2B // 0x2A is device address and a 1 is for read mode since we always read from our ADC
-
-void get_value() {
-    start_read(ADC_ADDR);
-}
